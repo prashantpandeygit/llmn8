c47cfa3dc78dca64d88c052adc4818b5e356dfa6
prashantpandeygit <prashantpandeyiuet@gmail.com>
2025-10-28 17:39:50 +0530

    add CORS to allow credentials

--- server/main.py
+++ server/main.py
@@ -67,6 +67,7 @@ app = FastAPI(title="microchat")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
